be1dfc906b99873c293d3a996881aec55b43d3f2
Akshada Shinde <Akshada Shinde@LAPTOP-NCAMLNOD>
2024-04-01 19:15:33 +0530

    datatype

---
+++ data/src/primitive/Pdata.java
@@ -0,0 +1,15 @@
+package primitive;
+
+public class Pdata {
+
+	public static void main(String[]args)
+	{
+	System.out.println(1+1);
+	System.out.println(1+'a');
+	System.out.println(1+1.1);
+	System.out.println(1+"hello");
+	System.out.println(true+"hi");
+	}
+	}
+
+
